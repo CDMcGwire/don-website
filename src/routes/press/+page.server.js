895c79a33f751df83b373acfcb6a65444545b0b1
Connor McGwire <CDMcGwire@users.noreply.github.com>
2023-10-09 20:06:16 -0700

    Fix to screenshot listing

--- src/routes/press/+page.server.js
+++ src/routes/press/+page.server.js
@@ -1,13 +1,10 @@
 ﻿/** @type {import('./$types').PageServerLoad} */
 export function load({params}) {
-	console.log("Hello??");
 	const imageModules = import.meta.glob("../../../static/scrn/*.jpg");
 	let paths = [];
 
 	for (const path in imageModules) {
-		imageModules[path]().then(({default: imgPath}) => {
-			paths.push(imgPath.substring(7));
-		});
+		paths.push(path.replace("../../../static", ""));
 	}
 	paths.sort((a, b) => a.localeCompare(b));
 	return {
